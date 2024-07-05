0966c36fe98ce0085b844d4d1017d559c69cf270
teddav <teddav@users.noreply.github.com>
2024-07-05 12:20:07 +0200

    enable basic safe module

--- test/utils.sol
+++ test/utils.sol
@@ -2,21 +2,18 @@
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
-import "safe-contracts/Safe.sol";
-import "safe-contracts/proxies/SafeProxy.sol";
-import {Counter} from "../contracts/Counter.sol";
+import {Safe} from "safe-contracts/Safe.sol";
+import {Enum} from "safe-contracts/common/Enum.sol";
+import {SafeProxy} from "safe-contracts/proxies/SafeProxy.sol";
 
-contract CounterTest is Test {
+contract TestUtils is Test {
     uint256[3] pks = [
         0x47e179ec197488593b187f80a00eb0da91f1b9d0b13f8733639f19c30a34926a,
         0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba,
         0x92db14e403b83dfe3df233f83dfa3a0d7096f21ca9b0d6d6b8d88b2b4ec1564e
     ];
 
-    function test_Deploy() public {
-        Counter counter = new Counter();
-        counter.setNumber(123);
-
+    function deployAndSetupSafe() internal returns (Safe) {
         address singleton = address(new Safe());
         Safe safe = Safe(payable(address(new SafeProxy(singleton))));
 
@@ -36,50 +33,12 @@ contract CounterTest is Test {
             payable(address(0))
         );
 
-        bytes memory tx_data = abi.encodeWithSelector(
-            Counter.setNumber.selector,
-            456
-        );
-        // console.logBytes(tx_data);
-
-        bytes32 to_sign = safe.getTransactionHash(
-            address(counter),
-            0,
-            tx_data,
-            Enum.Operation(0),
-            1_000_000,
-            1_000_000,
-            1_000_000,
-            address(0),
-            address(0),
-            0
-        );
-
-        bytes memory signatures = getSignature(to_sign);
-
-        assertEq(counter.number(), 123);
-
-        safe.execTransaction(
-            address(counter),
-            0,
-            tx_data,
-            Enum.Operation(0),
-            1_000_000,
-            1_000_000,
-            1_000_000,
-            address(0),
-            payable(address(0)),
-            signatures
-        );
-
-        assertEq(counter.number(), 456);
+        return safe;
     }
 
-    function getSignature(
-        bytes32 to_sign
-    ) internal view returns (bytes memory) {
-        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(pks[0], to_sign);
-        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(pks[1], to_sign);
+    function getSignature(bytes32 toSign) internal view returns (bytes memory) {
+        (uint8 v1, bytes32 r1, bytes32 s1) = vm.sign(pks[0], toSign);
+        (uint8 v2, bytes32 r2, bytes32 s2) = vm.sign(pks[1], toSign);
         bytes memory sig1 = abi.encodePacked(r1, s1, v1);
         bytes memory sig2 = abi.encodePacked(r2, s2, v2);
         bytes memory signatures = abi.encodePacked(sig1, sig2);
@@ -110,5 +69,35 @@ contract CounterTest is Test {
         // console.log("======");
 
         return signatures;
+    }
+
+    function execTransaction(Safe safe, address to, bytes memory data) public {
+        bytes32 toSign = safe.getTransactionHash(
+            to,
+            0,
+            data,
+            Enum.Operation(0),
+            1_000_000,
+            1_000_000,
+            1_000_000,
+            address(0),
+            address(0),
+            0
+        );
+
+        bytes memory signatures = getSignature(toSign);
+
+        safe.execTransaction(
+            to,
+            0,
+            data,
+            Enum.Operation(0),
+            1_000_000,
+            1_000_000,
+            1_000_000,
+            address(0),
+            payable(address(0)),
+            signatures
+        );
     }
 }
